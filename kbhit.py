6f17d67cf18c87cdb3700532d668826f3937d868
Daniel Manning <danielmanning213@gmail.com>
2022-09-13 14:01:36 +1000

    Handle both windows and mac

--- kbhit.py
+++ kbhit.py
@@ -1,10 +1,22 @@
-import win32event
-import win32api
-import msvcrt
+import os
 import sys
 import select
+from utils import is_windows
 
-hStdin = win32api.GetStdHandle(win32api.STD_INPUT_HANDLE);
+if is_windows:
+    import win32event
+    import win32api
+    import msvcrt
+
+    hStdin = win32api.GetStdHandle(win32api.STD_INPUT_HANDLE)
+
 
 def check_key():
-    return win32event.WaitForSingleObject(hStdin,1000) == win32event.WAIT_OBJECT_0 and msvcrt.kbhit()
+    if is_windows:
+        return (
+            win32event.WaitForSingleObject(hStdin, 1000) == win32event.WAIT_OBJECT_0
+            and msvcrt.kbhit()
+        )
+    else:
+        _, w, _ = select.select([], [sys.stdin], [], 0)
+        return len(w)
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import os
+
+
 def opcode(instruction):
     return instruction >> 12
 
@@ -9,4 +12,7 @@ def trapcode(instruction):
 def sign_extend(x, bit_count):
     if (x >> (bit_count - 1)) & 1:
         x |= 0xFFFF << bit_count
-    return x & 0xffff
+    return x & 0xFFFF
+
+
+is_windows = os.name == "nt"
