da6596073868db778be280bc15124d946d7d413e
mchldann <michael.dann@rmit.edu.au>
2023-05-07 19:36:36 +1000

    Modified some of the starter files for the new spec.

--- increment_blocks.asm
+++
@@ -1,4 +0,0 @@
-.ORIG x3000
-HALT
-LINE_LEN .FILL #5 ; Note: Please do not change the name of this constant
-.END
---
+++ totem_pole.asm
@@ -0,0 +1,4 @@
+.ORIG x3000
+HALT
+HEIGHT .FILL #20 ; Note: Please do not change the name of this constant
+.END
